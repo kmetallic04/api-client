60f67bd254cc895d8c700008ef6dbb2e75b8d38b
John Ward <john.ward@gro-intelligence.com>
2020-05-21 18:48:55 +0000

    refactor: monkeypatch requests.get to return mock data

--- conftest.py
+++ conftest.py
@@ -7,3 +7,27 @@ import requests
 @pytest.fixture(autouse=True)
 def no_requests(monkeypatch):
     monkeypatch.delattr("requests.sessions.Session.request")
+
+
+class MockResponse(requests.Response):
+    def __init__(self, status_code, response, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.status_code = status_code
+        self.mock_response = response
+
+    @staticmethod
+    def json():
+        return mock_response
+
+# monkeypatched requests.get moved to a fixture
+@pytest.fixture(autouse=True)
+def mock_response(monkeypatch):
+    """Requests.get() mocked to return {'mock_key':'mock_response'}."""
+
+    def mock_get(*args, **kwargs):
+        # TODO: detect what request is being made and return corresponding mock data
+        url = args[0]
+        params = kwargs['params']
+        return MockResponse()
+
+    monkeypatch.setattr(requests, "get", mock_get)
